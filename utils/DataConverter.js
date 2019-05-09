f2bad3b056aaf48ef1dec9652922b3a33dcf69f3
Jacob <jacob.t.middleton@googlemail.com>
2019-05-09 21:12:58 +0100

    add utils from other one

---
+++ utils/DataConverter.js
@@ -0,0 +1,49 @@
+export default class DataConverter {
+  constructor() {}
+
+  processDataForDay(data, day) {
+    const hoursForCodes = new Map();
+
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < data[i].length; j++) {
+        const row = data[i][j];
+        const hours = this.hoursOnDay(day, row.startTime, row.endTime);
+        const code = row.jira.timesheetCode;
+        if (hours > 0.08) {
+          if (!hoursForCodes.get(code)) {
+            hoursForCodes.set(code, 0);
+          }
+          hoursForCodes.set(code, hoursForCodes.get(code) + hours);
+        }
+      }
+    }
+
+    return hoursForCodes;
+  }
+
+  hoursOnDay(day, startTime, endTime) {
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const nineOclock = day.getTime() + 9 * 60 * 60 * 1000;
+    const five30 = day.getTime() + 17.5 * 60 * 60 * 1000;
+    let end = endTime.getTime();
+    let start = startTime.getTime();
+
+    if (day >= endTime) {
+      return 0;
+    }
+    if (day.getTime() + oneDay <= startTime.getTime()) {
+      return 0;
+    }
+
+    if (nineOclock > start) {
+      start = nineOclock;
+    }
+
+    if (five30 < end) {
+      end = five30;
+    }
+
+    return (end - start) / oneHour;
+  }
+}
---
+++ utils/statuses.js
@@ -0,0 +1,5 @@
+export const inQa = "In Progress: QA/Test";
+export const inDev = "In Progress: Development";
+export const codeReview = "Code Review";
+
+export const trackedStatuses = [inQa, inDev, codeReview];
